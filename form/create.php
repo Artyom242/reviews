bfaec58ca3e85e54ea718db878416ddd7c7e10cb
Artyom242 <artemp876@gmail.com>
2023-03-09 14:21:34 +0700

    правка

--- form/create.php
+++ form/create.php
@@ -1,6 +1,5 @@
 <?php
 /** @var PDO $db */
-
 $db = require_once $_SERVER['DOCUMENT_ROOT'] . '/db.php';
 
 $name = $_POST['name'];
@@ -8,7 +7,7 @@ $review = $_POST['review'];
 $data = date("y.m.d");
 $time = date("H:i:s");
 
-$query = $db -> prepare ('insert into reviews (name, review, data, time) values (:name, :review, :data, :time)');
+$query = $db->prepare ('insert into reviews (name, review, data, time) values (:name, :review, :data, :time)');
 $flag = $query->execute(['name' => $name, 'review' => $review, 'data' => $data, 'time' => $time]);
 
 header('Location: /mini_project?flag=' . $flag);
